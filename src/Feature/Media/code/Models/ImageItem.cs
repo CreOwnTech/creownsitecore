715a10131f11c2970552dd53ae752414d387080a
CreOwnTech <info@creowntech.com>
2017-05-26 17:52:37 +0530

    added carousel

---
+++ src/Feature/Media/code/Models/ImageItem.cs
@@ -0,0 +1,23 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using Glass.Mapper.Sc.Fields;
+using Sitecore.Foundation.GlassMapper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.Media.Models
+{
+    [SitecoreType(TemplateId = "{2877CE31-2742-458B-938D-070903B8A45B}", AutoMap = true)]
+    public class ImageItem : SitecoreItem
+    {
+        [SitecoreField("Title")]
+        public virtual string Title { get; set; }
+
+        [SitecoreField("Description")]
+        public virtual string Description { get; set; }
+
+        [SitecoreField("Image")]
+        public virtual Image Image { get; set; }
+    }
+}
